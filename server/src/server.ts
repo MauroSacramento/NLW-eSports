67db84ee2177168b763c482cb70e79a8a0800699
Maurosacramento <maurosacramento35@gmail.com>
2025-05-18 19:56:17 +0100

    First commit

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(3333, ()=> {
+    console.log("Servidor rodando na porta 3333")
+})
